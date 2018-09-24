79a45367ed79295a9d7e8777f6b417fa957badea
Tom Smeets <tom@tsmeets.nl>
2018-09-24 10:42:35 +0200

    Update stack.yaml

--- default.nix
+++ default.nix
@@ -1,14 +1,15 @@
 { pkgs ? import <nixpkgs> {}, haskellPackages ? pkgs.haskellPackages }:
 with pkgs.haskellPackages; haskellPackages.mkDerivation {
   pname = "FractalArt";
-  version = "0.2.0.3";
+  src = ./.;
+  version = "0.2.0.4";
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     base directory filepath JuicyPixels mwc-random primitive random
     vector
   ];
-  executableSystemDepends = with pkgs; [ xorg.libX11 ];
+  executableSystemDepends = with pkgs; [ xorg.libX11 zlib ];
   homepage = "https://github.com/TomSmeets/FractalArt";
   description = "Generates colorful wallpapers";
   license = stdenv.lib.licenses.mit;
